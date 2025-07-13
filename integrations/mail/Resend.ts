4332f7f26d5cee3c636c7afb839e23fec7ce6776
Gabriel <gabriel.rskm@gmail.com>
2025-07-12 23:36:05 -0300

    change resend file and add enviroment variable

--- integrations/mail/Resend.ts
+++ integrations/mail/Resend.ts
@@ -1,10 +1,12 @@
 import { Resend } from 'resend';
 const RESEND_TOKEN = Deno.env.get('RESEND_TOKEN');
+const RESEND_FROM_EMAIL = Deno.env.get('RESEND_FROM_EMAIL');
+
 const resend = new Resend(RESEND_TOKEN);
 
 export default async function sendEmail(email: string, subject: string, htmlContent: string) {
   await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
+    from: RESEND_FROM_EMAIL!,
     to: [email],
     subject: subject,
     html: htmlContent,
